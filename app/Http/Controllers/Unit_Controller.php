c355a673441f096b930ca843b82c02c7628dc2e9
tanujajagtap24 <jagtaptanuja2067@gmail.com>
2024-12-24 11:35:21 +0530

    main

---
+++ app/Http/Controllers/Unit_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Unit_Controller extends Controller
+{
+    //
+}
--- resources/views/admin/unit/create.blade.php
+++ resources/views/admin/unit/create.blade.php
@@ -10,8 +10,8 @@
       <div class="row mb-2">
         <div class="col-12">
           <div class="d-flex justify-content-between">
-            <h1> Create New Tax</h1>
-            <a href="/admin/tax/list" class="btn btn-success"> Tax List</a>
+            <h1> Create New Unit</h1>
+            <a href="/admin/tax/list" class="btn btn-success"> Unit List</a>
           </div>
 
         </div>
@@ -30,14 +30,14 @@
           <!-- general form elements -->
           <div class="card ">
             <!-- form start -->
-            <form action="/admin/tax/store" method="post">
+            <form action="/admin/unit/store" method="post">
               @csrf
               <div class="card-body">
                 <div class="form-group">
                   <div class="row">
                     <div class="col-lg-12">
-                      <label for="">Tax Percentage</label>
-                      <input type="text" class="form-control" id="tax_percent" name="tax_percent">
+                      <label for="">Unit Name</label>
+                      <input type="text" class="form-control" id="unit_nm" name="unit_nm">
                     </div>
                   </div>
                 </div>
