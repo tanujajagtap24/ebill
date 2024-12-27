dee5973f3402c0ca0e7c5bd6d26c93ecf068b708
SujataBondre13 <sujatabondre2000@gmail.com>
2024-12-27 11:23:11 +0530

    product create page

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -34,46 +34,42 @@
               <div class="card-body">
                 <div class="form-group">
                   <div class="row">
-                    <div class="col-lg-8">
+                    <div class="col-lg-6">
                       <label for="">Product Name</label>
                       <input type="text" class="form-control" id="product_name" name="product_name">
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
+                      <label for="">HSN Code</label>
+                      <input type="text" class="form-control" id="hsn" name="hsn" placeholder="Enter HSN Code">
+                    </div>
+                  </div>
+                  <div class="row pt-3">
+                    <div class="col-lg-3">
                       <div class="d-flex justify-content-between">
                         <label for="">Category</label>
                         <a href="/admin/category/create" class="btn btn-success"> <i class="nav-icon fas fa-plus "></i> </a>
                       </div>
-                      <select name="product_cat" id="product_cat" class="form-control">
+                      <select name="p_cat" id="p_cat" class="form-control">
                         <option value=""> Select Category  </option>
                         @foreach ($catData as $data)
                           <option value="{{ $data->Category_Name }}">  {{ $data->Category_Name }}  </option>
                         @endforeach
                       </select>                    
                     </div>
-                  </div>
-                  <div class="row pt-3">
-                    <div class="col-lg-4">
-                      <label for="">Quantity</label> 
-                      <input type="text" class="form-control" id="qty" name="qty" oninput="Final()">
-                    </div>
-                    <div class="col-lg-4">
-                      <label for="">Rate</label>
-                      <input type="text" class="form-control" id="rate" name="rate" oninput="Final()">
-                    </div>
-                    <div class="col-lg-4">
-                      <label for="">Total</label>
-                      <input type="text" class="form-control" id="total" name="total" readonly>
-                    </div>
-                  </div>
-                  <div class="row pt-3">
+
                     <div class="col-lg-3">
-                      <label for="">Discount %</label>
-                      <input type="text" class="form-control" id="dis_percent" name="dis_percent" oninput="Final()">
+                      <div class="d-flex justify-content-between">
+                        <label for="">Brand</label>
+                        <a href="/admin/brand/create" class="btn btn-success"> <i class="nav-icon fas fa-plus "></i> </a>
+                      </div>
+                      <select name="p_brand" id="p_brand" class="form-control">
+                        <option value=""> Select Brand  </option>
+                        @foreach ($catData as $data)
+                          <option value="{{ $data->Category_Name }}">  {{ $data->Category_Name }}  </option>
+                        @endforeach
+                      </select>                    
                     </div>
-                    <div class="col-lg-3">
-                      <label for="">Discount Value</label>
-                      <input type="text" class="form-control" id="dis_value" name="dis_value" readonly>
-                    </div>
+
                     <div class="col-lg-3">
                       <label for="">Tax %</label>
                       <select name="tax_percent" id="tax_percent" class="form-control" oninput="Final()">
@@ -83,19 +79,116 @@
                         @endforeach
                       </select>
                     </div>
+
+                    <div class="col-lg-3">
+                      <label for="">Tax Type</label>
+                      <select name="tax_type" id="tax_type" class="form-control">
+                        <option value=""> Select Tax Type  </option>
+                        <option value="Inclusive Tax"> Inclusive Tax </option>
+                        <option value="Exclusive Tax"> Exclusive Tax </option>
+                      </select>
+                    </div>
+                  </div>
+
+                <div class="row pt-3">
+                  <div class="col-lg-4">
+                    <label for=""> Primary Unit </label>
+                    <select name="p_unit" id="p_unit" class="form-control" >
+                      <option value=""> Select  </option>
+                      <option value="Nos"> Nos </option>
+                      <option value="Kg"> Kg </option>
+                      <option value="Litre"> Litre </option>
+                    </select>
+                  </div>
+
+                  <div class="col-lg-4">
+                    <label for=""> Primary Unit </label>
+                    <select name="a_unit" id="a_unit" class="form-control" >
+                      <option value=""> Select </option>
+                      <option value="Nos"> Nos </option>
+                      <option value="Kg"> Kg </option>
+                      <option value="Litre"> Litre </option>
+                    </select>
+                  </div>
+
+                  <div class="col-lg-4">
+                    <label for=""> Conversion Factor  </label>
+                    <input type="text" name="c_factor" id="c_factor" placeholder="Enter Conversion Factor">
+                  </div>
+
+                  <div class="row pt-3">
+                    <div class="col-lg-4">
+                      <label for="">Barcode</label> 
+                      <input type="text" class="form-control" id="barcode" name="barcode">
+                    </div>
+                    <div class="col-lg-4">
+                      <label for="">Rate</label>
+                      <input type="text" class="form-control" id="rate" name="rate" oninput="Final()">
+                    </div>
+                    <div class="col-lg-4">
+                      <label for="">Quantity</label> 
+                      <input type="text" class="form-control" id="qty" name="qty" oninput="Final()">
+                    </div>
+                    <div class="col-lg-4">
+                      <label for="">Total</label>
+                      <input type="text" class="form-control" id="total" name="total" readonly>
+                    </div>
+                    <div class="col-lg-3">
+                      <label for="">Discount %</label>
+                      <input type="text" class="form-control" id="dis_percent" name="dis_percent" oninput="Final()">
+                    </div>
+                    <div class="col-lg-3">
+                      <label for="">Discount Value</label>
+                      <input type="text" class="form-control" id="dis_value" name="dis_value" readonly>
+                    </div>
                     <div class="col-lg-3">
                       <label for="">Final Value</label>
                       <input type="text" class="form-control" id="fin_value" name="fin_value" readonly>
                     </div>
+                    <div class="col-lg-3">
+                      <label for="">MFG Date</label>
+                      <input type="text" class="form-control" id="mfg" name="mfg" readonly>
+                    </div>
+                    <div class="col-lg-3">
+                      <label for="">Expiry Date</label>
+                      <input type="text" class="form-control" id="exp" name="exp" readonly>
+                    </div>
+
                   </div>
                 </div>
               </div>
               <!-- /.card-body -->
 
               <div class="card-footer text-center">
-                <button type="submit" class="btn btn-success">Submit</button>
-                <button type="reset" class="btn btn-primary">Reset</button>
-                <a href="/admin/product/list" class="btn btn-secondary"> Back</a>
+                <table>
+                  <tr>
+                    <th>Barcode</th>
+                    <th>Rate</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th>Discount %</th>
+                    <th>Discount Value</th>
+                    <th>Final Value</th>
+                    <th>Mfg Date</th>
+                    <th>Exp Date</th>
+                    <th>Action</th>
+                  </tr>
+                  <tr>
+                    
+                    <td> <input type="text" class="form-control" id="barcode" name="barcode"> </td>
+                    <td> <input type="text" class="form-control" id="rate" name="rate" oninput="Final()"> </td>
+                    <td> <input type="text" class="form-control" id="qty" name="qty" oninput="Final()"> </td>
+                    <td> <input type="text" class="form-control" id="total" name="total" readonly> </td>
+                    <td> <input type="text" class="form-control" id="dis_percent" name="dis_percent" oninput="Final()"> </td>
+                    <td> <input type="text" class="form-control" id="dis_value" name="dis_value" readonly> </td>
+                    <td> <input type="text" class="form-control" id="fin_value" name="fin_value" readonly> </td>
+                    <td> <input type="date" class="form-control" id="mfg" name="mfg" readonly> </td>
+                    <td> <input type="date" class="form-control" id="exp" name="exp" readonly> </td>
+                    <td> <a href=""> Button </a> </td>
+                  </tr>
+                </table>
+                <button type="submit" class="btn btn-success"><i class="nav-icon fas fa-save"> </i> Save</button>
+                <a href="/admin/product/list" class="btn btn-secondary"><i class="nav-icon fas fa-times "></i> Close</a>
               </div>
             </form>
           </div>
