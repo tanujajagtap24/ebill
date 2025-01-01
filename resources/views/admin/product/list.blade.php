842b48cbb32f1f74d3bffb3f8439f3a3f54acd32
tanujajagtap24 <jagtaptanuja2067@gmail.com>
2025-01-01 10:10:21 +0530

    list

--- resources/views/admin/product/list.blade.php
+++ resources/views/admin/product/list.blade.php
@@ -45,10 +45,6 @@
                                     <th>Quantity</th>
                                     <th>Rate</th>
                                     <th>Discount %</th>
-<<<<<<< HEAD
-                                    
-=======
->>>>>>> c628c52925275b248ee3d9aa9b6664c2ae20b0b0
                                     <th>Final Value</th>
                                     <th>Action</th>
                                 </thead>
@@ -57,44 +53,6 @@
                                         @foreach ($childData as $child)
                                         @if ($master->id == $child->product_master_id)
                                         <tr>
-<<<<<<< HEAD
-                                            <td rowspan="4"> {{ $loop->iteration }} </td>
-                                            <td rowspan="4"> {{ $data->Product_Name }} </td>
-                                            <td rowspan="4"> {{ $data->Product_Category }} </td>
-                                            <td rowspan="4">{{ $data->Product_Brand }}</td>
-                                            <td rowspan="4"> {{ $data->Tax_Percent }} </td>
-                                
-                                            <td> {{ $data->Quantity_1 }} </td>
-                                            <td> {{ $data->Rate_1 }} </td>
-                                            <td> {{ $data->Final_Value_1 }} </td>
-                                            <td> {{ $data->Dis_Percent_1 }} </td>
-                                            
-                                            <td rowspan="4">
-                                              <a class="btn btn-primary" href="/admin/product/view/{{ $data->id }}"> <i class="nav-icon fas fa-eye"></i> View </a>
-                                              <a class="btn btn-secondary" href="/admin/product/edit/{{ $data->id }}"> <i class="nav-icon fas fa-edit"></i> Edit </a>
-                                              <a class="btn btn-danger" href="/admin/product/delete/{{ $data->id }}"> <i class="nav-icon fas fa-trash"></i> Delete </a>
-                                            </td>
-                                          </tr>
-
-                                          <tr>
-                                            <td> {{ $data->Quantity_2 }} </td>
-                                            <td> {{ $data->Rate_2 }} </td>
-                                            <td> {{ $data->Dis_Percent_2 }} </td>
-                                            <td> {{ $data->Final_Value_2 }} </td>
-                                          </tr>
-                                          <tr>
-                                            <td> {{ $data->Quantity_3 }} </td>
-                                            <td> {{ $data->Rate_3 }} </td>
-                                            <td> {{ $data->Dis_Percent_3 }} </td>
-                                            <td> {{ $data->Final_Value_3 }} </td>
-                                          </tr>
-                                          <tr>
-                                            <td> {{ $data->Quantity_4 }} </td>
-                                            <td> {{ $data->Rate_4 }} </td>
-                                            <td> {{ $data->Dis_Percent_4 }} </td>
-                                            <td> {{ $data->Final_Value_4 }} </td>
-                                          </tr>
-=======
                                             {{-- <td rowspan="4"> {{ $loop->iteration }} </td> --}}
                                             <td rowspan="4"> {{ $master->id }} </td>
 
@@ -139,7 +97,6 @@
 
 
                                     @endforeach
->>>>>>> c628c52925275b248ee3d9aa9b6664c2ae20b0b0
                                     @endforeach
                                 </tbody>
 
