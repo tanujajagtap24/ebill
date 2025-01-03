a90afdaaa02e6dea54557b34e447ae8762eb63ad
SujataBondre13 <sujatabondre2000@gmail.com>
2025-01-03 14:26:58 +0530

    pos master

---
+++ database/migrations/2025_01_03_085053_create_tbl_pos_master_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblPosMasterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_pos_master', function (Blueprint $table) {
+            $table->id();
+            $table->string('Customer_id');
+            $table->string('Payment_Term');
+            $table->string('Total');
+            $table->date('Bill_Date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_pos_master');
+    }
+}
