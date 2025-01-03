18a174cfa4738f87f408225a8f36bf6c8b7ca030
tanujajagtap24 <jagtaptanuja2067@gmail.com>
2025-01-03 14:27:21 +0530

    pos child

---
+++ app/Models/pos_child.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pos_child extends Model
+{
+    public $timestamps = false;
+    protected $table='tbl_pos_child';
+}
---
+++ database/migrations/2025_01_03_085054_create_tbl_pos_child_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblPosChildTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_pos_child', function (Blueprint $table) {
+            $table->id();
+            $table->string('pos_master_id');
+            $table->string('Product_id');
+            $table->string('Sale_Price');
+            $table->string('Quantity');
+            $table->string('Total');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_pos_child');
+    }
+}
